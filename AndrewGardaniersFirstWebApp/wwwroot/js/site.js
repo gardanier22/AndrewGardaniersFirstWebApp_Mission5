508b43d68d7fbad1e46abe8c7e46f0e4dcb045cb
gardanier22 <agardanier22@gmail.com>
2024-02-09 20:42:55 -0700

    Add project files.

---
+++ AndrewGardaniersFirstWebApp/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//$(document).ready(function () {
+ //   $("#calculateTotal").click(function () {
+   //     var hours = parseInt($("#hours").val(), 10); 
+   //     var hourlyRate = parseFloat($("#hourlyCharge").val()); 
+
+   //     if (!isNaN(hours) && !isNaN(hourlyRate) && hours > 0) {
+   //         var totalCharge = hours * hourlyRate;
+   //         $("#totalCharge").val("$" + totalCharge.toFixed(2)); 
+   //     } else {
+   //         $("#totalCharge").val("Invalid Input");
+   //     }
+  //  });
+//});
